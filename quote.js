33a36834bfb367c360fbd114266841651e282db9
marjan <marjan.arod@gmail.com>
2019-01-13 13:26:13 -0800

    Changing URL from http https

--- quote.js
+++ quote.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	var author;
 	function newQuote (){
 		$.ajax({
-			url: "http://api.forismatic.com/api/1.0/",
+			url: "https://api.forismatic.com/api/1.0/",
 			jsonp: "jsonp",
 			dataType: "jsonp",
 			data: {
